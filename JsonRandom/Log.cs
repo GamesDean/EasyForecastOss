eb94380e00d7909f6ce7b33c9dff876d8477bb1e
stefano spinucci <virgo977virgo@gmail.com>
2016-08-17 04:53:28 +0200

    add Log class

---
+++ JsonRandom/Log.cs
@@ -0,0 +1,13 @@
+﻿namespace EasyForecast.SymEngine.Json
+{
+    public class Log
+    {
+        /// <summary>Log a message</summary>
+        /// <remarks>Side effects: write log</remarks>
+        /// <param name="MessageToLog">Message to log</param>
+        public void LogMessage(string MessageToLog)
+        {
+            // TODO implement log
+        }
+    }
+}
