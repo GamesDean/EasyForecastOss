13935c398fc9de19768ed51b3415255bcef3cd61
stefano spinucci <virgo977virgo@gmail.com>
2016-08-19 01:05:19 +0200

    changed namespace of Json Models

--- JsonRandom/JsonModels/JsonInputModel.cs
+++ JsonRandom/JsonModels/JsonInputModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EasyForecast.SymEngine.Json
+namespace EasyForecast.SymEngine.JsonModels
 {
     // TODO if useful modify IList<> properties implementing suggestion of rule "CA2227: Collection properties should be read only" https://msdn.microsoft.com/library/ms182327.aspx?cs-save-lang=1&cs-lang=csharp )
     public class NumParameter
