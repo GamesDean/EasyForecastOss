8220923206ca2d371ddbceba718fc4ca1a772350
stefano spinucci <virgo977virgo@gmail.com>
2016-08-19 01:05:45 +0200

    log class edit (minor implementation) + namespace change

---
+++ JsonRandom/Logs/MessageLog.cs
@@ -0,0 +1,21 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace EasyForecast.SymEngine.Logs
+{
+    public class MessageLog
+    {
+        /// <summary>Log a message. Side effects: write log. Failure when 'MessageToLog' is empty.</summary>
+        /// <param name="MessageToLog">Message to log</param>
+        /// <returns>Return object with status.</returns>
+        public Result LogMessage(string MessageToLog)
+        {
+            // TODO implement log
+
+            if (MessageToLog == "")
+                return Result.Fail("input paramater is empty");
+
+            return Result.Ok();
+        }
+    }
+}
+
